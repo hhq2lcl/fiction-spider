88b5d127f85c6784eae9763e3411e216b0ef2585
hhq2lcl <34256880+hhq2lcl@users.noreply.github.com>
2017-12-05 14:08:46 +0800

    Update and rename shengxu-spider.md to shengxu-spider.py

--- shengxu-spider.py
+++ shengxu-spider.py
@@ -1,7 +1,5 @@
-# fiction-spider
 import urllib.request
 from bs4 import BeautifulSoup
-
 url='http://www.biquge.com.tw/11_11850/'
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 req=urllib.request.Request(url,headers=headers)
