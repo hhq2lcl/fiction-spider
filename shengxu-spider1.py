434da139aade994a20a2f8e6c60af21a7ce58715
hhq2lcl <34256880+hhq2lcl@users.noreply.github.com>
2017-12-06 09:10:20 +0800

    Update shengxu-spider1.py

--- shengxu-spider1.py
+++ shengxu-spider1.py
@@ -1,16 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
 #圣墟小说目录地址
-url='http://www.biquge.com.tw/11_11850/'
-#User-agent
+url='http://www.biquge.com.tw/11_11850/'  #url是需要爬取的网页
+#User-agent-模拟浏览器
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-req=urllib.request.Request(url,headers=headers)
-res=urllib.request.urlopen(req)
-html=res.read().decode('gbk')
+req=urllib.request.Request(url,headers=headers)     #请求
+res=urllib.request.urlopen(req)         #打开req
+html=res.read().decode('gbk')           #读取与译码
 #创建BeautifulSoup对象
-soup=BeautifulSoup(html,'lxml')
+soup=BeautifulSoup(html,'lxml')         #这里是将content内容转化为BeautifulSoup格式的数据
 #搜索文档树，找出div标签中的id为list的所有子标签
-chapters=soup.find_all('div',id='list')
+chapters=soup.find_all('div',id='list')   #通过tag的id属性搜索标签
+#chapters=soup.find_all('div',{'id':'list'})—— #通过字典的形式搜索标签内容，返回的为一个列表[]
+
 #使用查询结果再创建一个BeautifulSoup对象,对其继续进行解析
 lists=BeautifulSoup(str(chapters),'lxml')
 #创建TXT文件夹
